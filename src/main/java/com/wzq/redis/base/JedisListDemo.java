fa18f46528a811fa2ca63b59776427a5d3f1a0bb
zhenqing.wang <wzqwsrf@126.com>
2016-06-21 10:35:06 +0800

    add List

--- src/main/java/com/wzq/redis/base/JedisListDemo.java
+++ src/main/java/com/wzq/redis/base/JedisListDemo.java
@@ -1,6 +1,9 @@
 package com.wzq.redis.base;
 
+import redis.clients.jedis.BinaryClient;
 import redis.clients.jedis.Jedis;
+
+import java.util.List;
 
 /**
  * Author: zhenqing.wang <wangzhenqing1008@163.com>
@@ -17,14 +20,76 @@ public class JedisListDemo {
 
     public void push() {
         String key = "company";
-        jedis.lpush(key, "baidu", "tencent", "taobao");
-        System.out.println(jedis.llen(key));
+        jedis.del(key);
+        long len = jedis.lpush(key, "baidu", "tencent", "taobao"); //lpush and rpush
+        System.out.println(len);
+        List<String> list = jedis.lrange(key, 0, -1);
+        System.out.println(list);
+
+        len = jedis.lpush(key, "cc");
+        System.out.println(len);
+
+        len = jedis.rpush(key, "a", "b", "c");
+        System.out.println(len);
+        list = jedis.lrange(key, 0, -1);
+        System.out.println(list);
+
+        len = jedis.lpushx(key, "key");
+        System.out.println(len);
+        len = jedis.rpushx(key, "key");
+        System.out.println(len);
+
+        len = jedis.lpushx("key", "key");
+        System.out.println(len);
+
+        len = jedis.rpushx("key", "key");
+        System.out.println(len);
+    }
+
+    public void pop() {
+        String key = "company";
         System.out.println(jedis.lrange(key, 0, -1));
+        String top = jedis.lpop(key);
+        System.out.println(top);
+        top = jedis.rpop(key);
+        System.out.println(top);
+
+        List<String> list = jedis.blpop(1000, key);
+        System.out.println(list);
+        jedis.rpush(key, "blpop");
+    }
+
+    public void other() {
+        String key = "company";
+        long num = jedis.lrem(key, 0, "key");
+        System.out.println(num);
+        jedis.lset(key, 0, "black"); //"OK"
+        String value = jedis.lindex(key, 0);
+        System.out.println(value);
+        List<String> list = jedis.lrange(key, 0, -1);
+        System.out.println(list);
+        jedis.ltrim(key, 0, -2);
+        list = jedis.lrange(key, 0, -1);
+        System.out.println(list);
+
+        jedis.linsert(key, BinaryClient.LIST_POSITION.BEFORE, "baidu", "didi");
+        list = jedis.lrange(key, 0, -1);
+        System.out.println(list);
+
+        jedis.linsert(key, BinaryClient.LIST_POSITION.AFTER, "baidu", "didi");
+        list = jedis.lrange(key, 0, -1);
+        System.out.println(list);
+
+        jedis.rpoplpush(key, "key");
+        System.out.println(jedis.lrange(key, 0, -1));
+        System.out.println(jedis.lrange("key", 0, -1));
+
     }
 
     public static void main(String[] args) {
         JedisListDemo jedisListDemo = new JedisListDemo();
         jedisListDemo.init();
         jedisListDemo.push();
+        jedisListDemo.other();
     }
 }
